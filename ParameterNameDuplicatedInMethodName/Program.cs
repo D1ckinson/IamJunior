a4118ec13123688d5681fc7fb9a238674e122934
D1ckinson <124173489+D1ckinson@users.noreply.github.com>
2025-03-27 16:10:54 +0300

    1.0 ParameterNameDuplicatedInMethodName

--- ParameterNameDuplicatedInMethodName/Program.cs
+++ ParameterNameDuplicatedInMethodName/Program.cs
@@ -1,36 +1,44 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Numerics;
+using System.Runtime.CompilerServices;
 
 namespace ParameterNameDuplicatedInMethodName
 {
     internal class Program
     {
-        private const int MinusOne = -1;
+        private readonly IEnumerable<string> _collection = [];
 
         private static void Main() { }
 
-        public void Shoot(Player player)
+        public void Shoot(IDamageable damageable)
         {
-            player.ThrowIfNull();
+            damageable.ThrowIfNull();
+            damageable.GetDamage();
         }
 
-        public string FindIndex(IEnumerable<int> collection, int number)
+        public string GetBy(int index)
         {
-            collection.ThrowIfNull();
+            index.ThrowIfNegative();
 
-            for (int i = 0; i < collection.Count(); i++)
-            {
-                if (collection.ElementAt(i) == number)
-                    return i.ToString();
-            }
+            int maxIndex = _collection.Count() - 1;
+            index.ThrowIfGreaterThan(maxIndex);
 
-            return MinusOne.ToString();
+            return _collection.ElementAt(index);
         }
     }
 
-    public class Player { }
+    public interface IDamageable
+    {
+        public void GetDamage();
+    }
 
     public static class ThrowHelper
     {
+        public static void ThrowIfNegative<T>(this T argument, [CallerArgumentExpression(nameof(argument))] string? name = null) where T : INumberBase<T> =>
+            ArgumentOutOfRangeException.ThrowIfNegative(argument, name);
+
+        public static void ThrowIfGreaterThan<T>(this T argument, T value, [CallerArgumentExpression(nameof(argument))] string? name = null) where T : IComparable<T> =>
+            ArgumentOutOfRangeException.ThrowIfGreaterThan(argument, value, name);
+
         public static void ThrowIfNull<T>(this T argument, [CallerArgumentExpression(nameof(argument))] string? name = null) where T : class =>
             ArgumentNullException.ThrowIfNull(argument, name);
     }
